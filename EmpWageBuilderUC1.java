398aeb027f203e85c6a8032a2fc9c3d96b31bf3a
kunallad54 <kunallad5473@gmail.com>
2021-06-09 15:25:21 +0530

    Employee Wage for month until conidition of total hours or clearar

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,8 +1,8 @@
 public class EmpWageBuilderUC1{
 	public static void main(String [] args){
-		//int IS_ABSENT = 0;
-		//int IS_FULLTIME = 1;
-		//int IS_PART_TIME = 2;
+		int IS_ABSENT = 0;
+		int IS_FULLTIME = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		int FULL_DAY = 8;
 		int HALF_DAY = 4;
@@ -11,30 +11,37 @@ public class EmpWageBuilderUC1{
 		int presentCount = 0;
 		int absentCount = 0;
 		int halfDayCount = 0;
-		for(int i=1;i<=20;i++){
+		int totalWorkingHours = 0;
+		int i=1;
+		while(i<=20){
 			int empCheck = (int) (Math.floor(Math.random() * 10)) % 3;
-			switch(empCheck){
-				case 0:
-					absentCount++;
-                                	break;
-				case 1:
-                                	wageCalculator = WAGE_PER_HOUR * FULL_DAY;
-					salary = salary + wageCalculator;
-					presentCount++;
-					break;
-				case 2:
-                        		wageCalculator = WAGE_PER_HOUR * HALF_DAY;
-					salary = salary + wageCalculator;
-					halfDayCount++;
-					break;
-				default:
-					System.out.println("ERROR OCCURED");
-					break;
+			if(empCheck == IS_ABSENT)
+				absentCount++;
+			else if(empCheck == IS_FULLTIME){
+                                wageCalculator = WAGE_PER_HOUR * FULL_DAY;
+				salary = salary + wageCalculator;
+				presentCount++;
+				totalWorkingHours += FULL_DAY;
+				if(totalWorkingHours == 100)
+					i=20;
 			}
+			else if(empCheck == IS_PART_TIME){
+                        	wageCalculator = WAGE_PER_HOUR * HALF_DAY;
+				salary = salary + wageCalculator;
+				halfDayCount++;
+				totalWorkingHours += HALF_DAY;
+				if(totalWorkingHours == 100)
+					i=20;
+			}
+			else{
+				System.out.println("ERROR OCCURED");
+			}
+			i++;
 		}
 		System.out.println("Employee was present for : "+presentCount+" days");
 		System.out.println("Employee was absent for : "+absentCount+" days");
 		System.out.println("Employee was present for half day for: "+halfDayCount+" days");
+		System.out.println("Total Working Hours: "+totalWorkingHours);
 		System.out.println("Salary for the month is : "+salary+ " Rs/-");
 	}
 }
