1fa5114695d38db7bedd385f0094106073232d43
kunallad54 <kunallad5473@gmail.com>
2021-06-09 12:44:10 +0530

    Employee wage for part time added

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,18 +1,25 @@
 public class EmpWageBuilderUC1{
 	public static void main(String [] args){
+		int IS_ABSENT = 0;
 		int IS_FULLTIME = 1;
 		int WAGE_PER_HOUR = 20;
 		int FULL_DAY = 8;
+		int HALF_DAY = 4;
 		int wageCalculator;
-		int empCheck =(int) (Math.floor(Math.random() * 10)) % 2;
+		int empCheck =(int) (Math.floor(Math.random() * 10)) % 3;
 		if (empCheck == IS_FULLTIME){
 			System.out.println("Employee is Present");
 			wageCalculator = WAGE_PER_HOUR * FULL_DAY;
 			System.out.println("Employee Wage for the day is : "+wageCalculator);
 		}
-		else{
+		else if( empCheck == IS_ABSENT){
 			System.out.println("Employee is Absent");
 			System.out.println("Employee Wage for the day is zero");
+		}
+		else{
+			System.out.println("Employee was present for half day");
+			wageCalculator = WAGE_PER_HOUR * HALF_DAY;
+			System.out.println("Employee wage for part time is : "+wageCalculator);
 		}
 	}
 }
