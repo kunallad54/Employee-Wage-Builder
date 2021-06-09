18d9d1b29efd07bb046bd6871ef846c71e2ec4b2
kunallad54 <kunallad5473@gmail.com>
2021-06-09 12:33:38 +0530

    Employee daily wage calculated

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,10 +1,18 @@
 public class EmpWageBuilderUC1{
 	public static void main(String [] args){
 		int IS_FULLTIME = 1;
+		int WAGE_PER_HOUR = 20;
+		int FULL_DAY = 8;
+		int wageCalculator;
 		int empCheck =(int) (Math.floor(Math.random() * 10)) % 2;
-		if (empCheck == IS_FULLTIME)
+		if (empCheck == IS_FULLTIME){
 			System.out.println("Employee is Present");
-		else
+			wageCalculator = WAGE_PER_HOUR * FULL_DAY;
+			System.out.println("Employee Wage for the day is : "+wageCalculator);
+		}
+		else{
 			System.out.println("Employee is Absent");
+			System.out.println("Employee Wage for the day is zero");
+		}
 	}
 }
