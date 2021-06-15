fb151e5192fc4aee8c9585323c2449f50dfa8de2
kunallad54 <kunallad5473@gmail.com>
2021-06-15 12:46:31 +0530

    Employee Wage for multiple companies calculated

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,80 +1,113 @@
-public class EmployeeWageBuilder{
+/**
+ * This project computes Employee Wage for multiple companies - Note: Each Company has its own wage, number of working days
+ * and working hours per month:
+ * This project calculates Employee Wage for Two Companies : 1.DMART 2.Reliance Retail
+ * For DMART : wage per hour = 20,number of working days = 20, maximum working hours = 100
+ * For Reliance Retail : wage per hour = 10,number of working days = 30, maximum working hours = 200
+ * It can calculate employee wage for multiple companies just need to create object for that company and give details
+ *
+ * @author Krunal Lad
+ * @Since 15-06-2021
+ */
 
-    public static final int ABSENT = 0;
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
+public class EmployeeWageBuilder {
 
-    // main method
-    public static void main(String[] args) {
+    // declaring instance variables
+    private static final int ABSENT = 0;
+    private static final int FULL_TIME = 1;
+    private static final int PART_TIME = 2;
+    private final String companyName;
+    private final int wagePerHour;
+    private final int numberOfWorkingDays;
+    private final int maxWorkingHours;
 
-        System.out.println("Welcome to Employee Wage Computation Problem !!!");
-        System.out.println();
-        employeeAttendanceChecker();
+    // creating constructor
+    private EmployeeWageBuilder(String companyName, int wagePerHour, int numberOfWorkingDays, int maxWorkingHours) {
+
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.numberOfWorkingDays = numberOfWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
 
     }
 
-    // checks employee attendance and  displays salary of employee
-    public static void employeeAttendanceChecker() {
+    //main method
+    public static void main(String[] args) {
 
-        int numberOfWorkingDays = 0;
-        int totalWorkingHours = 0;
-        int salary =0;
-        int fulTimeCount = 0;
-        int partTimeCount = 0;
+        System.out.println("Welcome to Employee Wage Computation Program !!! ");
+
+        // creating object for Dmart company and storing their details regarding employee wage
+        EmployeeWageBuilder dmart = new EmployeeWageBuilder("D-MART", 20, 20, 100);
+
+        // creating object for Reliance Retail company and storing their details regarding employee wage
+        EmployeeWageBuilder reliance = new EmployeeWageBuilder("Reliance Retail", 10, 30, 200);
+
+        employeeAttendanceChecker(dmart.companyName, dmart.wagePerHour, dmart.numberOfWorkingDays, dmart.maxWorkingHours);
+        System.out.println();
+        employeeAttendanceChecker(reliance.companyName, reliance.wagePerHour, reliance.numberOfWorkingDays, reliance.maxWorkingHours);
+    }
+
+    // Checks employee attendance and calculates Salary
+    private static void employeeAttendanceChecker(String company, int hourlyWage, int totalDays, int maxHours) {
+
         int absentCount = 0;
+        int fullTimeCount = 0;
+        int partTimeCount = 0;
+        int totalWorkingHours = 0;
+        int salary = 0;
 
-        while (numberOfWorkingDays != 20) {
+        // checks attendance and calculates salary for month or total working days
+        for (int i = 1; i <= totalDays; i++) {
 
-            int empHours = 0;
+            int empHrs = 0;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
             switch (empCheck) {
 
-                case ABSENT ->{
+                case ABSENT -> {
                     absentCount++;
-                    empHours = 0;
+                    empHrs = 0;
                 }
 
                 case FULL_TIME -> {
-                    fulTimeCount++;
-                    empHours = 8;
+                    fullTimeCount++;
+                    empHrs = 8;
                 }
 
                 case PART_TIME -> {
                     partTimeCount++;
-                    empHours = 4;
+                    empHrs = 4;
                 }
+            }
+            totalWorkingHours += empHrs;
 
+            if (totalWorkingHours >= maxHours) {
+                totalWorkingHours = maxHours;
+                break;
             }
 
-            totalWorkingHours += empHours;
-
-            if(totalWorkingHours >= 100){
-
-		totalWorkingHours = 100;
-                break;
-
-	    }
-
-            int wage = wageCalculator(empHours);
-            salary += wage;
-            numberOfWorkingDays++;
+            int wageForTheDay = wageCalculator(hourlyWage, empHrs);
+            salary += wageForTheDay;
 
         }
 
-        System.out.println("Absent : "+absentCount+" Days");
-        System.out.println("Full Time Present : "+fulTimeCount+" Days");
-        System.out.println("Part Time Present : "+partTimeCount+" Days");
-        System.out.println("Employee total working hours : "+totalWorkingHours);
-        System.out.println("Employee Salary for the month is : " + salary + " Rs /-");
+        displayInfo(company, absentCount, fullTimeCount, partTimeCount, totalWorkingHours, salary);
 
     }
 
-    // returns the wage of employee per day
-    public static int wageCalculator(int workingHours) {
+    // displays all information on console
+    public static void displayInfo(String cName, int aCount, int fTCount, int pTCount, int tHours, int sal) {
 
-        int WAGE_PER_HOUR = 20;
-        return WAGE_PER_HOUR * workingHours;
+        System.out.println("Following gives the Employee Wage Details for the company : " + cName);
+        System.out.println("Employee Absent : " + aCount + " days");
+        System.out.println("Employee Full Time : " + fTCount + " days");
+        System.out.println("Employee Part Time : " + pTCount + " days");
+        System.out.println("Total Working Hours : " + tHours + " hours");
+        System.out.println("Employee Salary is : " + sal + " Rs /-");
 
+    }
+
+    // returns wage for the day
+    public static int wageCalculator(int hourlyWage, int empHrs) {
+        return hourlyWage * empHrs;
     }
 }
