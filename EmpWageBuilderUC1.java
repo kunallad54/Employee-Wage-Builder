73c26cb638018d60c9a5f0a6555e5188ac4423d3
kunallad54 <kunallad5473@gmail.com>
2021-06-15 10:18:51 +0530

    Refactored the code using class methods and variables

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,47 +1,68 @@
 public class EmpWageBuilderUC1{
-	public static void main(String [] args){
-		int IS_ABSENT = 0;
-		int IS_FULLTIME = 1;
-		int IS_PART_TIME = 2;
-		int WAGE_PER_HOUR = 20;
-		int FULL_DAY = 8;
-		int HALF_DAY = 4;
-		int wageCalculator;
-		int salary = 0;
-		int presentCount = 0;
-		int absentCount = 0;
-		int halfDayCount = 0;
-		int totalWorkingHours = 0;
-		int i=1;
-		while(i<=20){
-			int empCheck = (int) (Math.floor(Math.random() * 10)) % 3;
-			if(empCheck == IS_ABSENT)
-				absentCount++;
-			else if(empCheck == IS_FULLTIME){
-                                wageCalculator = WAGE_PER_HOUR * FULL_DAY;
-				salary = salary + wageCalculator;
-				presentCount++;
-				totalWorkingHours += FULL_DAY;
-				if(totalWorkingHours == 100)
-					i=20;
-			}
-			else if(empCheck == IS_PART_TIME){
-                        	wageCalculator = WAGE_PER_HOUR * HALF_DAY;
-				salary = salary + wageCalculator;
-				halfDayCount++;
-				totalWorkingHours += HALF_DAY;
-				if(totalWorkingHours == 100)
-					i=20;
-			}
-			else{
-				System.out.println("ERROR OCCURED");
-			}
-			i++;
-		}
-		System.out.println("Employee was present for : "+presentCount+" days");
-		System.out.println("Employee was absent for : "+absentCount+" days");
-		System.out.println("Employee was present for half day for: "+halfDayCount+" days");
-		System.out.println("Total Working Hours: "+totalWorkingHours);
-		System.out.println("Salary for the month is : "+salary+ " Rs/-");
-	}
+
+    public static final int ABSENT = 0;
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Problem !!!");
+        System.out.println();
+        employeeAttendanceChecker();
+
+    }
+
+    public static void employeeAttendanceChecker() {
+
+        int numberOfWorkingDays = 0;
+        int totalWorkingHours = 0;
+        int salary =0;
+        int fulTimeCount = 0;
+        int partTimeCount = 0;
+        int absentCount = 0;
+        while (numberOfWorkingDays != 20) {
+            int empHours = 0;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+            switch (empCheck) {
+
+                case ABSENT ->{
+                    absentCount++;
+                    empHours = 0;
+                }
+
+                case FULL_TIME -> {
+                    fulTimeCount++;
+                    empHours = 8;
+                }
+
+                case PART_TIME -> {
+                    partTimeCount++;
+                    empHours = 4;
+                }
+
+            }
+            totalWorkingHours += empHours;
+            if(totalWorkingHours >= 100){
+		totalWorkingHours = 100;
+                break;
+	    }
+            int wage = wageCalculator(empHours);
+            salary += wage;
+            numberOfWorkingDays++;
+        }
+        System.out.println("Absent : "+absentCount+" Days");
+        System.out.println("Full Time Present : "+fulTimeCount+" Days");
+        System.out.println("Part Time Present : "+partTimeCount+" Days");
+        System.out.println("Employee total working hours : "+totalWorkingHours);
+        System.out.println("Employee Salary for the month is : " + salary + " Rs /-");
+
+    }
+
+    public static int wageCalculator(int workingHours) {
+
+        int WAGE_PER_HOUR = 20;
+        return WAGE_PER_HOUR * workingHours;
+
+    }
 }
